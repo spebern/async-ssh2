2ce5aadd4c5acc48a353e7759690b26fa22c40d6
bold <bold@cryptoguru.com>
2019-12-30 15:41:20 +0100

    remove unused loop

--- src/util.rs
+++ src/util.rs
@@ -11,19 +11,16 @@ macro_rules! into_the_future {
             type Output = Result<R, Error>;
 
             fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-                loop {
-                    let res = (&mut self.cb)();
-                    match res {
-                        Err(e) if e.code() == -37 => {
-                            if let Some(ref aio) = *self.aio {
-                                // TODO get rid of unwrap
-                                aio.set_waker(cx).unwrap();
-                            }
-                            return Poll::Pending;
+                match (&mut self.cb)() {
+                    Err(e) if e.code() == -37 => {
+                        if let Some(ref aio) = *self.aio {
+                            // TODO get rid of unwrap
+                            aio.set_waker(cx).unwrap();
                         }
-                        Err(e) => return Poll::Ready(Err(e)),
-                        Ok(val) => return Poll::Ready(Ok(val)),
+                        return Poll::Pending;
                     }
+                    Err(e) => return Poll::Ready(Err(e)),
+                    Ok(val) => return Poll::Ready(Ok(val)),
                 }
             }
         }
